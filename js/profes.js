2e63ecc9baaf839b530640efb61eddc52e090ec4
IsabelCarolina <isabel.alvarez@ug.uchile.cl>
2025-06-03 15:21:41 -0400

    Update profes.js

--- js/profes.js
+++ js/profes.js
@@ -497,7 +497,7 @@ async function datos(criterio) {
     }
 }
 
-datos("Dueñas Santander, Rodrigo").catch((error) => console.error(error));
+datos("Reyes León, Daniel").catch((error) => console.error(error));
 
 document.querySelectorAll("select")[0].addEventListener("change", (event) => {
     tabla.innerHTML = " ";
